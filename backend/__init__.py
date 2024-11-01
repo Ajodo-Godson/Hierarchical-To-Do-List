805b06ba62dced4a60bc5d35624de126787fd021
Ajodo Godson <godsonajodo2020@gmail.com>
2024-11-01 16:52:57 -0700

    removed undefined routes.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -37,11 +37,9 @@ def create_app():
 
     # Register blueprints
     from .auth import auth as auth_bp
-    from .routes import main as main_bp
     from .todo import todo as todo_bp
 
     app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
     app.register_blueprint(todo_bp)
 
     # Create database tables if they do not exist
